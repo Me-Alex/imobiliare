1e9d7c8cf2abda36c66ac7a9d5c7b4c30b24bd34
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2026-04-25 14:32:51 +0300

    fix: remove app/admin/layout.tsx

--- app/admin/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ro">
-      <body className="bg-bg-primary text-text-primary">{children}</body>
-    </html>
-  )
-}
