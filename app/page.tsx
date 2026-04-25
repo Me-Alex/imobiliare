5c8d8f5875bbba981ee7b1cce0a5c51c9667e17a
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2026-04-25 13:41:52 +0300

    add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import ProprietatiSection from '@/components/ProprietatiSection'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <ProprietatiSection />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
