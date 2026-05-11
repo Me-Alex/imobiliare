5a75ef87b175b326952151fa374c85fee1244119
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2026-05-11 17:31:55 +0300

    Adaug app/sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,5 @@
+export default function sitemap() {
+  const base = 'https://hqsimobiliare.ro'
+  const routes = ['/', '/proprietati', '/despre', '/contact', '/comparare', '/favorite']
+  return routes.map((route) => ({ url: `${base}${route}`, lastModified: new Date() }))
+}
