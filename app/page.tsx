b25c81b6902879d47d4363f10b12a124ade8a2e7
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2026-05-11 06:21:42 +0300

    Accesibilizez homepage-ul

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <main>
+    <main id="continut">
       <Header />
       <Hero />
       <PropertyHighlights />
